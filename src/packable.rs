5b868b4895d7c42b0f97e96e7d12503a0c9d9934
Max Kofler <kofler.max.dev@gmail.com>
2024-10-12 15:46:16 +0200

    Add trait PackableClass: Allow for 32 and 64 bit packing

--- src/packable.rs
+++ src/packable.rs
@@ -1,5 +1,7 @@
 use std::io::{self};
 
+use crate::model::Class;
+
 /// An error while unpackin
 #[derive(Debug)]
 pub enum UnpackError {
@@ -43,6 +45,32 @@ pub trait Packable: Sized {
     fn unpack<R: io::Read>(r: &mut R, big_endian: bool) -> Result<Self, UnpackError>;
 }
 
+/// A special form of the [Packable] trait - a pointer that can be stored in `32` or `64` bits
+pub trait PackableClass: Sized {
+    /// Pack `self` to `w`
+    /// # Arguments
+    /// * `w` - The stream to write to using `.write_all()`
+    /// * `big_endian` - Whether the stream should be written to in big endian form
+    /// * `class` - The ELF class to use for packing
+    fn pack_class<W: io::Write>(
+        self,
+        w: &mut W,
+        big_endian: bool,
+        class: Class,
+    ) -> Result<(), io::Error>;
+
+    /// Unpack `Self` from `r`
+    /// # Arguments
+    /// * `r` - The stream to read from
+    /// * `big_endian` - Whether the stream should be read from in big endian form
+    /// * `class` - The ELF class to use for unpacking
+    fn unpack_class<R: io::Read>(
+        r: &mut R,
+        big_endian: bool,
+        class: Class,
+    ) -> Result<Self, UnpackError>;
+}
+
 macro_rules! impl_packable {
     ($i:ident) => {
         impl Packable for $i {
@@ -79,3 +107,30 @@ impl_packable!(u64);
 impl_packable!(i64);
 impl_packable!(u128);
 impl_packable!(i128);
+
+impl PackableClass for u64 {
+    fn pack_class<W: io::Write>(
+        self,
+        w: &mut W,
+        big_endian: bool,
+        class: Class,
+    ) -> Result<(), io::Error> {
+        if class == Class::ELF64 {
+            self.pack(w, big_endian)
+        } else {
+            (self as u32).pack(w, big_endian)
+        }
+    }
+
+    fn unpack_class<R: io::Read>(
+        r: &mut R,
+        big_endian: bool,
+        class: Class,
+    ) -> Result<Self, UnpackError> {
+        Ok(if class == Class::ELF64 {
+            Self::unpack(r, big_endian)?
+        } else {
+            u32::unpack(r, big_endian)? as u64
+        })
+    }
+}
